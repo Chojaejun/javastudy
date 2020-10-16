a273c746c024f97651111b0dd0514e2ed84fd0fd
jjj <jognny12@naver.com>
2020-10-16 17:45:14 +0900

    *

---
+++ VariableEx/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,30 @@
+package kr.co.ezen;
+
+public class MainEx {
+
+	public static void main(String[] args) {
+		
+		// "morning"을 msg4 변수에 대입하시고
+		// 코드에 이를 적용하세요.
+		// 변수 선언 공식
+		// 자료형 변수명 = 자료형에 맞는 값;
+		
+		String msg = "hello";
+		String msg2 = "hi";
+		String msg3 = "good";
+		String msg4 = "morning";
+		
+		System.out.println(msg);
+		System.out.println(msg2);
+		System.out.println(msg3);
+		System.out.println(msg4);
+		System.out.println(msg);
+		System.out.println(msg3);
+		System.out.println(msg2);
+		System.out.println(msg);
+		System.out.println(msg+msg2);
+	
+		
+	}
+	
+}

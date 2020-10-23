2ce42f03993cbc1afc2656604302b4d0d964680f
jjj <jognny12@naver.com>
2020-10-23 17:56:15 +0900

    수업

---
+++ ConstructorEx/src/kr/co/ezen/Dog.java
@@ -0,0 +1,20 @@
+package kr.co.ezen;
+
+public class Dog {
+	private String	dsn;
+	private String	name;
+	public String getDsn() {
+		return dsn;
+	}
+	public void setDsn(String dsn) {
+		this.dsn = dsn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}

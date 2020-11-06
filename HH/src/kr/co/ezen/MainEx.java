eac93bf2536d4afac59dc0496ca7b1c611711e08
jjj <jognny12@naver.com>
2020-11-06 17:39:20 +0900

    수업

---
+++ HH/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,22 @@
+package kr.co.ezen;
+
+import com.naver.Bow;
+import com.naver.Gun;
+import com.naver.Monster;
+import com.naver.Sword;
+import com.naver.Weapon;
+
+public class MainEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Monster mon = new Monster(100);
+		System.out.println(mon);
+		
+		Weapon[] weapons = {new Bow("È°",1), new Sword("°Ë",5), new Gun("ÃÑ",10)};
+		
+		Weapon w = weapons
+	}
+
+}
